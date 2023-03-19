087f080235b4cb74a378d213ac1cc3a27c8ff3e4
Max <maxmwangikenya@gmail.com>
2023-03-19 12:04:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+
+destructivelyPrependCat('Bob');
+
+
+function destructivelyRemovelastcat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstcat(){
+    return cats.shift();
+}
+destructivelyRemoveFirstcat();
+function appendCat(name){
+    return[...cats,name];
+}
+appendCat("broom");
+
+function prependCat(name){
+    return[name, ...cats];
+}
+prependCat("Arnold");
+
+function removeLastCat(){
+    return cats.slice(0,cats.length-1);
+}
+removeLastCat();
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
